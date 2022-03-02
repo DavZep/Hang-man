959e25b2a068cc2bae2ccef94fd695fa76d5e1c2
DavZep <93220182+DavZep@users.noreply.github.com>
2022-03-01 20:35:59 -0800

    cleaned up spacing + seudoCode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,10 +25,10 @@ var totalGuesses = 0
 var lettersGuessed = []
 //word as dashes
 var gameWordDashes = ""
-
+//correct guess sound
 let correctSound = new Audio("./assets/audio/correct_answer.mp3");
 correctSound.volume = 0.3;
-
+//eliminated sound
 let wrongSound = new Audio("./assets/audio/wrong_answer.mp3");
 wrongSound.volume = 0.2;
 
@@ -46,15 +46,15 @@ var userGuessDiv = document.getElementById("user-guesses")
         //correct answers do not apply
         totalGuesses = gameWord.length
         //function see below
-        createDashes()
+        createDashes();
         wordDiv.textContent = gameWordDashes
         totalGuessDiv.textContent = totalGuesses
-        userGuessDiv.textContent = lettersGuessed.join("")
-        console.log(gameWord)
+        userGuessDiv.textContent = lettersGuessed.join("");
+        console.log(gameWord);
     }
 
 //creates dashes where it loops the gameWord (totalGuesses is the lenght here) 
-// and creates _'s for every letter ex: dog : _ _ _
+// and creates _'s for every letter ex: dog will look like  _ _ _
 function createDashes() {
     for (let i = 0; i < totalGuesses; i++) {
         gameWordDashes = gameWordDashes + "_"
@@ -69,13 +69,13 @@ document.onkeyup = function (event) {
     messageDiv.textContent = ""
 
     //Condition if theres any gameword dashes left (for checkGameWon)
-if (totalGuesses > 0 && gameWordDashes.search("_") !== -1) {
-    console.log(gameWordDashes)
-    console.log(gameWordDashes.search("_") !== -1)
+    if (totalGuesses > 0 && gameWordDashes.search("_") !== -1) {
+    console.log(gameWordDashes);
+    console.log(gameWordDashes.search("_") !== -1);
     //This is a Condition which checks if list letterGuessed has the UserKey in it already
     //*check if letter has already been used*
     if (lettersGuessed.indexOf(userKey) !== -1) {
-        messageDiv.textContent = '"' + userKey + '" is already been picked'
+        messageDiv.textContent = '"' + userKey + '" has already been picked'
     }else {
         //loops through the indices and check how many times the userKey is found in gameWord
         var indices =[];
@@ -88,54 +88,56 @@ if (totalGuesses > 0 && gameWordDashes.search("_") !== -1) {
             //loops through and joins/puts  the letter pressed and replaces _'s with a correct letter *only if its part of the word*
             for (let j = 0; j < indices.length; j++) {
                 var currentIndex = indices[j]
-                gameWordDashes = gameWordDashes.split("") //creates an array of the gameword (adds a comma after every letter/space as a string)
+                gameWordDashes = gameWordDashes.split(""); //creates an array of the gameword (adds a comma after every letter/space as a string)
                 gameWordDashes[currentIndex] = userKey
-                gameWordDashes = gameWordDashes.join("")
+                gameWordDashes = gameWordDashes.join("");
                 checkGameWon()
             }
 
             //records/saves the letters press by the user to letterGuessed empty array to stop from multiple same guesses
             lettersGuessed.push(userKey)
 
-            wordDiv.textContent = gameWordDashes
+            wordDiv.textContent = gameWordDashes//writing to elemnts on html the as they go 
             totalGuessDiv.textContent = totalGuesses
-            userGuessDiv.textContent = lettersGuessed.join("")
+            userGuessDiv.textContent = lettersGuessed.join("");
         
         } else {
             totalGuesses-- //subtracts 1 from the total number of guesses(which is the random word length)
-            lettersGuessed.push(userKey) //records/saves the letters press by the user to letterGuessed empty array
+            lettersGuessed.push(userKey); //records/saves the letters press by the user to letterGuessed empty array
 
             totalGuessDiv.textContent = totalGuesses
             messageDiv.textContent = '"' + userKey + '" is wrong... choose again!'
-            checkGameLost()
+            checkGameLost();
         }
     }
-} else {
-    console.log("hit")
-    checkGameWon()
-    checkGameLost()
-    wordDiv.classList.add("hide")
-    totalGuessDiv.classList.add("hide")
-    userGuessDiv.classList.add("hide")
-    
+// condition runs when gameWon or gameLost is triggered which hides elements
+    } else {
+        console.log("hit")//debugg where in the code, condition wasnt hitting
+        checkGameWon();
+        checkGameLost();
+        wordDiv.classList.add("hide");
+        totalGuessDiv.classList.add("hide");
+        userGuessDiv.classList.add("hide");
+    }
 }
-}
-//checks if game is won by 
+//checks if game is won 
 function checkGameWon() {
     if (gameWordDashes === gameWord) {
         messageDiv.textContent = 'Winner! "' + gameWord + '" is the correct answer'
-        correctSound.play();
+        correctSound.play();//plays sound when word is guessed
     }
 
 }
 
+//check if number of totalGuesses is less than or equal to zero to end(or else it goes on forever)
 function checkGameLost() {
     if (totalGuesses <= 0) {
-        messageDiv.textContent = 'Loser!... press Re-Play to try again "' + gameWord + '" is the correct answer'
-        wrongSound.play();
+        messageDiv.textContent = 'Loser!...press Re-Play to try again. "' + gameWord + '" is the correct answer'
+        wrongSound.play(); //plays sound when total guesses reaches zero
     }
 }
 
+//resets game...sets values to to begining of a game (not refresh page)
 function reset() {
     gameWord = ""
     totalGuesses = 0
@@ -144,13 +146,13 @@ function reset() {
     wordDiv.textContent = ""
     totalGuessDiv.textContent = ""
     userGuessDiv.textContent = ""
-    wordDiv.classList.remove("hide")
-    totalGuessDiv.classList.remove("hide")
-    userGuessDiv.classList.remove("hide")
-    playGame()
+    wordDiv.classList.remove("hide");
+    totalGuessDiv.classList.remove("hide");
+    userGuessDiv.classList.remove("hide");
+    playGame();
 }
 
-playGame()   //calling function playGame we created
+playGame();  //calling function playGame we created
 console.log(gameWordDashes);
 console.log(gameWord);
 
