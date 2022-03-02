069321c8b471474952af5b694f40dc9edec7e43b
DavZep <93220182+DavZep@users.noreply.github.com>
2022-03-01 18:39:18 -0800

    check win/lose + sound

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,20 +18,28 @@ if letter is not in the word the comp lets user know.  they then get a strike/gu
 var wordBank = ["ratatouille", "dune", "spiderman", "venom", "spacejam",
     "toystory", "godzilla", "wonderwoman", "borat", "starwars", "thehungergames",
     "ted", "tron", "inception", "avatar", "jurrassicpark", "matrix", "terminator"]
-    var gameWord = ""
-    //gameWord length
-    var totalGuesses = 0
-    // list of letters the user has guessd
-    var lettersGuessed = []
-    //word as dashes
-    var gameWordDashes = ""
-    
-    var messageDiv = document.getElementById("message")
-    var wordDiv = document.getElementById("game-word")
-    var totalGuessDiv = document.getElementById("total-guesses")
-    var userGuessDiv = document.getElementById("user-guesses")
+var gameWord = ""
+//gameWord length
+var totalGuesses = 0
+// list of letters the user has guessd
+var lettersGuessed = []
+//word as dashes
+var gameWordDashes = ""
+
+let correctSound = new Audio("./assets/audio/correct_answer.mp3");
+correctSound.volume = 0.3;
+
+let wrongSound = new Audio("./assets/audio/wrong_answer.mp3");
+wrongSound.volume = 0.2;
+
+//variables that will be used to write to the HTML id's for each (grabs placeholder)
+var messageDiv = document.getElementById("message")
+var wordDiv = document.getElementById("game-word")
+var totalGuessDiv = document.getElementById("total-guesses")
+var userGuessDiv = document.getElementById("user-guesses")
     
     function playGame() {
+        messageDiv.textContent = ""
         //this chooses a random word from gameBank array
         gameWord = wordBank[Math.floor(Math.random() * wordBank.length)];
         //creates a number of incorrect guesses determined by length of the word ex:dog 3 chances
@@ -42,6 +50,7 @@ var wordBank = ["ratatouille", "dune", "spiderman", "venom", "spacejam",
         wordDiv.textContent = gameWordDashes
         totalGuessDiv.textContent = totalGuesses
         userGuessDiv.textContent = lettersGuessed.join("")
+        console.log(gameWord)
     
     }
 
@@ -61,9 +70,11 @@ document.onkeyup = function (event) {
     messageDiv.textContent = ""
     //This is a Condition which checks if list letterGuessed has the UserKey in it already
     //*check if letter has already been used*
-
+if (totalGuesses > 0 && gameWordDashes.search("_") !== -1) {
+    console.log(gameWordDashes)
+    console.log(gameWordDashes.search("_") !== -1)
     if (lettersGuessed.indexOf(userKey) !== -1) {
-        messageDiv.textContent = userKey + " is already been picked"
+        messageDiv.textContent = '"' + userKey + '" is already been picked'
     }else {
         //loops through the indices and check how many times the userKey is found in gameWord
         var indices =[];
@@ -79,6 +90,7 @@ document.onkeyup = function (event) {
                 gameWordDashes = gameWordDashes.split("") //creates an array of the gameword (adds a comma after every letter/space as a string)
                 gameWordDashes[currentIndex] = userKey
                 gameWordDashes = gameWordDashes.join("")
+                checkGameWon()
             }
 
             //records/saves the letters press by the user to letterGuessed empty array to stop from multiple same guesses
@@ -93,15 +105,54 @@ document.onkeyup = function (event) {
             lettersGuessed.push(userKey) //records/saves the letters press by the user to letterGuessed empty array
 
             totalGuessDiv.textContent = totalGuesses
-            messageDiv.textContent = userKey + " is wrong, choose again!"
-
+            messageDiv.textContent = '"' + userKey + '" is wrong... choose again!'
+            checkGameLost()
         }
+    }
+} else {
+    console.log("hit")
+    checkGameWon()
+    checkGameLost()
+    wordDiv.classList.add("hide")
+    totalGuessDiv.classList.add("hide")
+    userGuessDiv.classList.add("hide")
+    
+}
+}
+
+function checkGameWon() {
+    if (gameWordDashes === gameWord) {
+        messageDiv.textContent = 'Winner! "' + gameWord + '" is the correct answer'
+        correctSound.play();
+    
     }
 
 }
 
+function checkGameLost() {
+    if (totalGuesses <= 0) {
+        messageDiv.textContent = 'Loser!... press Re-Play to try again "' + gameWord + '" is the correct answer'
+        wrongSound.play();
+    
+    }
+}
+
+function reset() {
+    gameWord = ""
+    totalGuesses = 0
+    lettersGuessed = []
+    gameWordDashes = ""
+    wordDiv.textContent = ""
+    totalGuessDiv.textContent = ""
+    userGuessDiv.textContent = ""
+    wordDiv.classList.remove("hide")
+    totalGuessDiv.classList.remove("hide")
+    userGuessDiv.classList.remove("hide")
+    playGame()
+}
+
 playGame()   //calling function playGame we created
-console.log(gameWordDashes)
-console.log(gameWord)
-console.log(totalGuesses) //guesses remaining
+console.log(gameWordDashes);
+console.log(gameWord);
+console.log(totalGuesses); //guesses remaining
 
